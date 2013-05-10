17eed9d13c385e5eb34947612e8a6f963010c918
chemerisuk <maksim.chemerisuk@strevus.com>
2013-05-10 16:59:57 +0300

    fix all found issues + refactoring

--- src/better-dateinput-polyfill.js
+++ src/better-dateinput-polyfill.js
@@ -22,131 +22,150 @@ DOM.extend("input[type=date]", {
             return "<div class='better-dateinput-calendar' hidden>" + content + "</div>";
         })()
     },
-    constructor: function() {
-        var input = this,
-            calendar = input.next();
+    constructor: (function() {
+        var makeCalendarDateSetter = function(calendar) {
+            var calendarCaption = calendar.find(".better-dateinput-calendar-header"),
+                calendarDays = calendar.findAll("[data-index]");
 
-        input
-            .set("type", "text") // remove legacy dateinput if it exists
-            .on({
-                input: function() {
-                    if (input.get("value")) input._syncDate();
-                },
-                click: function() {
-                    input._syncDate();
+            return function(value) {
+                var iterDate = new Date(value.getFullYear(), value.getMonth(), 0);
+                // update caption
+                calendarCaption.set("<span data-i18n='calendar.month." + value.getMonth() + "'> " + (isNaN(value.getFullYear()) ? "" : value.getFullYear()));
+                
+                if (!isNaN(iterDate.getTime())) {
+                    // move to begin of the start week
+                    iterDate.setDate(iterDate.getDate() - iterDate.getDay());
+                    
+                    calendarDays.each(function(day, index) {
+                        iterDate.setDate(iterDate.getDate() + 1);
+                        
+                        var mDiff = value.getMonth() - iterDate.getMonth(),
+                            dDiff = value.getDate() - iterDate.getDate();
 
-                    calendar.show();
-                },
-                keydown: function(e) {
-                    var key = e.get("keyCode"),
-                        delta = 0,
-                        currentDate = this.getDate();
-
-                    if (key === 27 || key === 9 || key === 13) {
-                        calendar.hide(); // enter, esc or tab key exits
-                    } else if (key === 8 || key === 46) {
-                        this.set(""); // backspace or delete clears the value
-                    } else {
-                        if (key === 74 || key === 40) { delta = 7; }
-                        else if (key === 75 || key === 38) { delta = -7; }                            
-                        else if (key === 76 || key === 39) { delta = 1; }
-                        else if (key === 72 || key === 37) { delta = -1; }
-
-                        if (delta) {
-                            if (e.get("altKey")) {
-                                currentDate.setMonth(currentDate.getMonth() + (delta > 0 ? 1 : -1));
-                            } else {
-                                currentDate.setDate(currentDate.getDate() + delta);
-                            }
-
-                            this.setDate(currentDate);
+                        if (value.getFullYear() !== iterDate.getFullYear()) {
+                            mDiff *= -1;
                         }
-                    }
 
-                    e.preventDefault();
+                        day.set("className", mDiff ?
+                            (mDiff > 0 ? "prev-calendar-day" : "next-calendar-day") :
+                            (dDiff ? "calendar-day" : "current-calendar-day")
+                        );
+
+                        day.set(iterDate.getDate().toString());
+                    });
+
+                    // update current date
+                    this.setData("calendarDate", value);
+                }
+
+                return this;
+            };
+        };
+
+        return function() {
+            var input = this,
+                calendar = input.next();
+
+            input
+                .set("type", "text") // remove legacy dateinput if it exists
+                .on({
+                    click: function() {
+                        this._syncDateWithCalendar(calendar);
+                    },
+                    keydown: function(e) {
+                        this._handleDateInputKeys(e.get("keyCode"), e.get("altKey"), calendar);
+                        
+                        e.preventDefault(); // prevent default typing of characters, submit on enter etc.
+                    }
+                });
+
+            calendar.on("click", function(e) {
+                input._handleCalendarClick(e.target, calendar);
+                
+                e.stopPropagation(); // stop bubbling to allow navigation via prev/next month buttons
+                e.preventDefault(); // prevent focusing on the input if it's inside of the label
+            });
+
+            DOM.on("click", function() {
+                if (!input.isFocused()) {
+                    calendar.hide();    
                 }
             });
 
-        calendar.on({
-            click: function(e) {
-                var target = e.target,
-                    calendarDate = input.getData("calendarDate"),
-                    currentYear = calendarDate.getFullYear(),
-                    currentMonth = calendarDate.getMonth(),
-                    currentDate = calendarDate.getDate(),
-                    targetDate;
+            this.setCalendarDate = makeCalendarDateSetter(calendar);
 
-                if (target.get("data-index")) {
-                    input.setDate(new Date(currentYear, currentMonth,
-                        parseInt(target.getData("index"), 10) + 3 -
-                            new Date(currentYear, currentMonth, 1).getDay()));
-
-                    calendar.hide();
-                } else if (target.hasClass("better-dateinput-calendar-prev")) {
-                    input.setDate(new Date(currentYear, currentMonth - 1, 1));
-                } else if (target.hasClass("better-dateinput-calendar-next")) {
-                    input.setDate(new Date(currentYear, currentMonth + 1, 1));
-                }
-
-                e.preventDefault();
-                e.stopPropagation();
+            // show calendar for autofocused elements
+            if (this.isFocused()) {
+                this.fire("focus");
             }
-        });
-
-        DOM.on("click", function() {
-            if (!input.isFocused()) {
-                calendar.hide();    
-            }
-        });
-
-        var container = calendar.find(".better-dateinput-calendar-days"),
-            containerCaption = calendar.find(".better-dateinput-calendar-header"),
-            containerDays = calendar.findAll("[data-index]");
-
-        this.setDate = function(value) {
-            var iterDate = new Date(value.getFullYear(), value.getMonth(), 0);
-            // update caption
-            containerCaption.set("<span data-i18n='calendar.month." + value.getMonth() + "'> " + (isNaN(value.getFullYear()) ? "" : value.getFullYear()));
-            
-            if (!isNaN(iterDate.getTime())) {
-                // move to begin of the start week
-                iterDate.setDate(iterDate.getDate() - iterDate.getDay());
-                
-                containerDays.each(function(day, index) {
-                    iterDate.setDate(iterDate.getDate() + 1);
-                    
-                    var mDiff = value.getMonth() - iterDate.getMonth(),
-                        dDiff = value.getDate() - iterDate.getDate();
-
-                    if (value.getFullYear() !== iterDate.getFullYear()) {
-                        mDiff *= -1;
-                    }
-
-                    day.set("className", mDiff ?
-                        (mDiff > 0 ? "prev-calendar-day" : "next-calendar-day") :
-                        (dDiff ? "calendar-day" : "current-calendar-day")
-                    );
-
-                    day.set(iterDate.getDate().toString());
-                });
-                // update current date
-                input.setData("calendarDate", value);
-            }
-
-            input.set("value", JSON.parse(JSON.stringify(value)).split("T")[0]);
         };
-
-        // show calendar for autofocused elements
-        if (this.isFocused()) {
-            this.fire("focus");
-        }
-    },
-    getDate: function() {
+    })(),
+    getCalendarDate: function() {
         return this.getData("calendarDate");
     },
-    _syncDate: function() {
+    _syncDateWithCalendar: function(calendar) {
         var value = (this.get("value") || "").split("-");
-        // switch calendar to appropriate date
-        this.setDate(value.length > 1 ? new Date( parseInt(value[0],10), parseInt(value[1],10) - 1, parseInt(value[2],10)) : new Date());
+        // switch calendar to the input value date
+        this.setCalendarDate(value.length > 1 ? new Date( parseInt(value[0],10), parseInt(value[1],10) - 1, parseInt(value[2],10)) : new Date());
+
+        calendar.show();
+    },
+    _syncDateWithInput: function(calendar) {
+        var date = this.getCalendarDate(),
+            zeroPadMonth = ("00" + (date.getMonth() + 1)).slice(-2),
+            zeroPadDate = ("00" + date.getDate()).slice(-2);
+
+        this.set(date.getFullYear() + "-" + zeroPadMonth + "-" + zeroPadDate);
+
+        calendar.hide();
+    },
+    _handleDateInputKeys: function(key, altKey, calendar) {
+        var delta = 0,
+            currentDate = this.getCalendarDate();
+
+        if (key === 13) { // show/hide calendar on enter key
+            if (calendar.get("hidden")) {
+                this._syncDateWithCalendar(calendar);
+            } else {
+                this._syncDateWithInput(calendar);
+            }
+        } else if (key === 27 || key === 9) {
+            calendar.hide(); // esc or tab key hides calendar
+        } else if (key === 8 || key === 46) {
+            this.set(""); // backspace or delete clears the value
+        } else {
+            if (key === 74 || key === 40) { delta = 7; }
+            else if (key === 75 || key === 38) { delta = -7; }                            
+            else if (key === 76 || key === 39) { delta = 1; }
+            else if (key === 72 || key === 37) { delta = -1; }
+
+            if (delta) {
+                if (altKey) {
+                    currentDate.setMonth(currentDate.getMonth() + (delta > 0 ? 1 : -1));
+                } else {
+                    currentDate.setDate(currentDate.getDate() + delta);
+                }
+
+                this.setCalendarDate(currentDate);
+            }
+        }
+    },
+    _handleCalendarClick: function(el, calendar) {
+        var calendarDate = this.getCalendarDate(),
+            currentYear = calendarDate.getFullYear(),
+            currentMonth = calendarDate.getMonth(),
+            currentDate = calendarDate.getDate();
+
+        if (el.get("data-index")) {
+            this.setCalendarDate(new Date(currentYear, currentMonth,
+                parseInt(el.getData("index"), 10) + 2 -
+                    new Date(currentYear, currentMonth, 1).getDay()));
+
+            this._syncDateWithInput(calendar);
+        } else if (el.hasClass("better-dateinput-calendar-prev")) {
+            this.setCalendarDate(new Date(currentYear, currentMonth - 1, 1)).fire("focus");
+        } else if (el.hasClass("better-dateinput-calendar-next")) {
+            this.setCalendarDate(new Date(currentYear, currentMonth + 1, 1)).fire("focus");
+        }
     }
 });
