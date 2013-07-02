917ddf0aa49a80137773ab2b9376e1b7bd8252ea
Guillaume Bartolini <guillaumebartolini@gmail.com>
2013-07-03 08:50:25 +1100

    Create better-dateinput-polyfill.fr.js
    French localization

---
+++ i18n/better-dateinput-polyfill.fr.js
@@ -0,0 +1,23 @@
+DOM.importStrings({
+    // days of week
+    "calendar.weekday.1": "Lun",
+    "calendar.weekday.2": "Mar",
+    "calendar.weekday.3": "Mer",
+    "calendar.weekday.4": "Jeu",
+    "calendar.weekday.5": "Ven",
+    "calendar.weekday.6": "Sam",
+    "calendar.weekday.7": "Dim",
+    // monthes
+    "calendar.month.0": "Janvier",
+    "calendar.month.1": "Février",
+    "calendar.month.2": "Mars",
+    "calendar.month.3": "Avril",
+    "calendar.month.4": "Mai",
+    "calendar.month.5": "Juin",
+    "calendar.month.6": "Juillet",
+    "calendar.month.7": "Août",
+    "calendar.month.8": "Septembre",
+    "calendar.month.9": "Octobre",
+    "calendar.month.10": "Novembre",
+    "calendar.month.11": "Decembre"
+});
