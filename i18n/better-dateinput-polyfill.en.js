10e9a7d60c039a3654889e5eeebff9b7a071a7ca
chemerisuk <chemerisuk@gmail.com>
2013-07-21 11:08:55 +0300

    2-letter days of week + more compact calendar

--- i18n/better-dateinput-polyfill.en.js
+++ i18n/better-dateinput-polyfill.en.js
@@ -1,12 +1,12 @@
 DOM.importStrings({
     // days of week
-    "calendar.weekday.1": "Mon",
-    "calendar.weekday.2": "Tue",
-    "calendar.weekday.3": "Wed",
-    "calendar.weekday.4": "Thu",
-    "calendar.weekday.5": "Fri",
-    "calendar.weekday.6": "Sat",
-    "calendar.weekday.7": "Sun",
+    "calendar.weekday.1": "Mo",
+    "calendar.weekday.2": "Tu",
+    "calendar.weekday.3": "We",
+    "calendar.weekday.4": "Th",
+    "calendar.weekday.5": "Fr",
+    "calendar.weekday.6": "Sa",
+    "calendar.weekday.7": "Su",
     // monthes
     "calendar.month.0": "January",
     "calendar.month.1": "February",
