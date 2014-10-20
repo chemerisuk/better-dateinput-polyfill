dc6d9b6ccee759345e1870581a35337dab83dd6a
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2014-10-20 10:38:10 -0200

    fix for #10: adding . to the end of compact month's keys

--- src/better-dateinput-polyfill.js
+++ src/better-dateinput-polyfill.js
@@ -75,7 +75,7 @@
                     // build RFC 1123 string based on the lang attribute
                     .append(DOM.create("span").i18n(I18N_DAYS[value.getUTCDay() ? value.getUTCDay() - 1 : 6]))
                     .append(",&nbsp;" + ((value.getUTCDate() > 9 ? "" : "0") + value.getUTCDate()) + "&nbsp;")
-                    .append(DOM.create("span").i18n(I18N_MONTHS[value.getUTCMonth()].substr(0, 3)))
+                    .append(DOM.create("span").i18n(I18N_MONTHS[value.getUTCMonth()].substr(0, 3) + "."))
                     .append("&nbsp;" + value.getUTCFullYear());
             } else {
                 value = new Date();
