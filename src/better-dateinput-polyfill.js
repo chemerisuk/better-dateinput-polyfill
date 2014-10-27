4780e9387f72841a37be29e4f0b2784e9f67ab0e
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-10-27 21:30:56 +0300

    update to better-dom 2, use boilerplate

--- src/better-dateinput-polyfill.js
+++ src/better-dateinput-polyfill.js
@@ -1,16 +1,15 @@
 (function(DOM, COMPONENT_CLASS, I18N_DAYS, I18N_MONTHS) {
     "use strict";
 
-    var htmlEl = DOM.find("html"),
-        ampm = function(pos, neg) { return htmlEl.get("lang") === "en-US" ? pos : neg },
+    var ampm = function(pos, neg) { return DOM.get("lang") === "en-US" ? pos : neg },
         formatISODate = function(value) { return value.toISOString().split("T")[0] };
 
     // need to skip mobile/tablet browsers
     DOM.extend("input[type=date]", !("orientation" in window), {
         constructor: function() {
-            var calendar = DOM.create("div.{0}>a[unselectable=on]*2+span[aria-hidden=true].{0}-header+table[aria-hidden=true].{0}-days>thead>tr>th[unselectable=on]*7+tbody>tr*6>td*7", [COMPONENT_CLASS + "-calendar"]),
+            var calendar = DOM.create("div.{0}>a[unselectable=on]*2+span[aria-hidden=true].{0}-header+table[aria-hidden=true].{0}-days>thead>(tr>th[unselectable=on]*7)+(tbody>tr*6>td*7)", [COMPONENT_CLASS + "-calendar"]),
                 displayedValue = DOM.create("span[aria-hidden=true].{0}-value", [COMPONENT_CLASS]),
-                color = this.style("color"),
+                color = this.css("color"),
                 offset = this.offset(),
                 calOffset;
 
@@ -20,9 +19,9 @@
                 .set({type: "text", value: Date.now()})
                 // hide original input text
                 // IE8 doesn't suport color:transparent - use background-color instead
-                .style("color", document.addEventListener ? "transparent" : this.style("background-color"))
+                .css("color", document.addEventListener ? "transparent" : this.css("background-color"))
                 // handle arrow keys, esc etc.
-                .on("keydown", this.onCalendarKeyDown.bind(this, calendar), ["which", "shiftKey"])
+                .on("keydown", ["which", "shiftKey"], this.onCalendarKeyDown.bind(this, calendar))
                 // sync picker visibility on focus/blur
                 .on(["focus", "click"], this.onCalendarFocus.bind(this, calendar))
                 .on("blur", this.onCalendarBlur.bind(this, calendar))
@@ -31,31 +30,31 @@
             calOffset = calendar.offset();
 
             calendar
-                .on("mousedown", this.onCalendarClick.bind(this, calendar))
-                .style({
+                .on("mousedown", ["target"], this.onCalendarClick.bind(this, calendar))
+                .css({
                     "margin-left": offset.left - calOffset.left + (offset.width - calOffset.width) / 2,
                     "margin-top": offset.bottom - calOffset.top,
-                    "z-index": 1 + (this.style("z-index") | 0)
+                    "z-index": 1 + (this.css("z-index") | 0)
                 })
                 .hide(); // hide calendar to trigger show animation properly later
 
             // move calendar to the top when passing cross browser window bounds
             if (DOM.find("html").get("clientHeight") < offset.bottom + calOffset.height) {
-                calendar.style("margin-top", calOffset.top - offset.bottom - calOffset.height);
+                calendar.css("margin-top", calOffset.top - offset.bottom - calOffset.height);
             }
 
             displayedValue
                 .on("click", this.onCalendarFocus.bind(this, calendar))
                 // copy input CSS
-                .style(this.style(["width", "font", "padding-left", "padding-right", "text-align", "border-width", "box-sizing"]))
-                .style({
+                .css(this.css(["width", "font", "padding-left", "padding-right", "text-align", "border-width", "box-sizing"]))
+                .css({
                     "color": color,
                     "line-height": offset.height + "px",
                     "margin-left": offset.left - calOffset.left,
                     "margin-top": offset.top - calOffset.top,
                 });
 
-            this.parent("form").on("reset", this.onFormReset.bind(this));
+            this.closest("form").on("reset", this.onFormReset.bind(this));
             this.watch("value", this.onValueChanged.bind(this, displayedValue,
                 calendar.find("." + COMPONENT_CLASS + "-calendar-header"), calendar.findAll("th"), calendar.findAll("td")));
             // trigger watchers to build the calendar
@@ -73,9 +72,9 @@
             if (value.getTime()) {
                 displayedValue
                     // build RFC 1123 string based on the lang attribute
-                    .append(DOM.create("span").i18n(I18N_DAYS[value.getUTCDay() ? value.getUTCDay() - 1 : 6]))
+                    .append(DOM.create("span").l10n(I18N_DAYS[value.getUTCDay() ? value.getUTCDay() - 1 : 6]))
                     .append(",&nbsp;" + ((value.getUTCDate() > 9 ? "" : "0") + value.getUTCDate()) + "&nbsp;")
-                    .append(DOM.create("span").i18n(I18N_MONTHS[value.getUTCMonth()].substr(0, 3) + "."))
+                    .append(DOM.create("span").l10n(I18N_MONTHS[value.getUTCMonth()].substr(0, 3) + "."))
                     .append("&nbsp;" + value.getUTCFullYear());
             } else {
                 value = new Date();
@@ -88,17 +87,17 @@
             // update calendar caption
             caption
                 .set("&nbsp;" + year)
-                .prepend(DOM.create("span").i18n(I18N_MONTHS[month]));
+                .prepend(DOM.create("span").l10n(I18N_MONTHS[month]));
             // update calendar weekday captions
-            weekdays.each(function(el, index) {
-                el.i18n(I18N_DAYS[ampm(index ? index - 1 : 6, index)]);
+            weekdays.forEach(function(el, index) {
+                el.l10n(I18N_DAYS[ampm(index ? index - 1 : 6, index)]);
             });
             // update calendar content
             iterDate = new Date(Date.UTC(year, month, 0));
             // move to beginning of current month week
             iterDate.setUTCDate(iterDate.getUTCDate() - iterDate.getUTCDay() - ampm(1, 0));
             // update day numbers
-            days.set("class", function(day) {
+            days.forEach((day) => {
                 iterDate.setUTCDate(iterDate.getUTCDate() + 1);
 
                 var mDiff = month - iterDate.getUTCMonth(),
@@ -106,11 +105,10 @@
 
                 if (year !== iterDate.getUTCFullYear()) mDiff *= -1;
 
-                day.set("-ts", iterDate.getTime()).set(iterDate.getUTCDate());
-
-                return mDiff ?
+                day.set("_ts", iterDate.getTime()).set(iterDate.getUTCDate());
+                day.set("class", mDiff ?
                     (mDiff > 0 ? COMPONENT_CLASS + "-calendar-past" : COMPONENT_CLASS + "-calendar-future") :
-                    (dDiff ? "" :  COMPONENT_CLASS + "-calendar-today");
+                    (dDiff ? "" :  COMPONENT_CLASS + "-calendar-today"));
             });
             // trigger event manually to notify about changes
             this.fire("change");
@@ -118,14 +116,14 @@
         onCalendarClick: function(calendar, target) {
             var targetDate;
 
-            if (target == "a") {
+            if (target.matches("a")) {
                 targetDate = new Date(this.get());
 
                 if (!targetDate.getTime()) targetDate = new Date();
 
                 targetDate.setUTCMonth(targetDate.getUTCMonth() + (target.next("a").length ? -1 : 1));
-            } else if (target == "td") {
-                targetDate = new Date(target.get("-ts"));
+            } else if (target.matches("td")) {
+                targetDate = new Date(target.get("_ts"));
                 calendar.hide();
             }
 
@@ -175,22 +173,22 @@
             calendar.hide();
         },
         onCalendarFocus: function(calendar) {
-            this.legacy(function(node) {
-                // use the trick below to reset text selection on focus
-                setTimeout(function() {
-                    if ("selectionStart" in node) {
-                        node.selectionStart = 0;
-                        node.selectionEnd = 0;
-                    } else {
-                        var inputRange = node.createTextRange();
+            var node = this[0];
 
-                        inputRange.moveStart("character", 0);
-                        inputRange.collapse();
-                        inputRange.moveEnd("character", 0);
-                        inputRange.select();
-                    }
-                }, 0);
-            });
+            // use the trick below to reset text selection on focus
+            setTimeout(function() {
+                if ("selectionStart" in node) {
+                    node.selectionStart = 0;
+                    node.selectionEnd = 0;
+                } else {
+                    var inputRange = node.createTextRange();
+
+                    inputRange.moveStart("character", 0);
+                    inputRange.collapse();
+                    inputRange.moveEnd("character", 0);
+                    inputRange.select();
+                }
+            }, 0);
 
             calendar.show();
         },
