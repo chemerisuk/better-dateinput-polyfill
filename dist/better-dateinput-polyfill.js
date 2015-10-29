a4ed1384435f834007850aca2e0a0316d7319cc5
Brewster <brewster1134@gmail.com>
2015-10-29 13:39:37 -0700

    data attribute to disable date picker
    we were looking at ways to use native date input behavior on a per input/form basis. how do you feel about a simple check if an input is explicitly disabled?
    
    ```html
    <input type="date" data-dateinput-disabled></input>
    ```

--- dist/better-dateinput-polyfill.js
+++ dist/better-dateinput-polyfill.js
@@ -19,6 +19,7 @@
     // need to skip mobile/tablet browsers
     DOM.extend("input[type=date]", testDateInput, {
         constructor: function() {var this$0 = this;
+            return if this$0.data('dateinput-disabled');
             var calendar = PICKER_TEMPLATE.clone(true),
                 label = LABEL_TEMPLATE.clone(true),
                 color = this.css("color");
