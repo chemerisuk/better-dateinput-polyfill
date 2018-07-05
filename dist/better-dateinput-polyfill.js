dc98d09eb54aafc52a9041d3ae388c140322ac65
Maksim Chemerisuk <chemerisuk@gmail.com>
2018-07-06 00:24:51 +0300

    3.0.0

--- dist/better-dateinput-polyfill.js
+++ dist/better-dateinput-polyfill.js
@@ -1,6 +1,6 @@
 /**
  * better-dateinput-polyfill: input[type=date] polyfill for better-dom
- * @version 3.0.0-rc.2 Thu, 14 Jun 2018 09:33:29 GMT
+ * @version 3.0.0 Thu, 05 Jul 2018 21:24:51 GMT
  * @link https://github.com/chemerisuk/better-dateinput-polyfill
  * @copyright 2018 Maksim Chemerisuk
  * @license MIT
@@ -8,6 +8,8 @@
 (function (DOM, VK_SPACE, VK_TAB, VK_ENTER, VK_ESCAPE, VK_BACKSPACE, VK_DELETE, VK_CONTROL) {
   "use strict";
   /* globals html:false */
+
+  var CLICK_EVENT_TYPE = "orientation" in window ? "touchend" : "mousedown";
 
   var HTML = DOM.get("documentElement"),
       ampm = function ampm(pos, neg) {
@@ -68,7 +70,7 @@
   }
 
   var SVG_TEMPLATE = DOM.create('<svg xmlns="http://www.w3.org/2000/svg"><text dominant-baseline="central" y="50%"></text></svg>');
-  var PICKER_TEMPLATE = DOM.create('<div tabindex="-1" class="btr-dateinput-picker"><object data="about:blank" type="text/html" width="100%" height="100%"></object></div>');
+  var PICKER_TEMPLATE = DOM.create('<dateinput-picker tabindex="-1"><object data="about:blank" type="text/html" width="100%" height="100%"></object></dateinput-picker>');
   var PICKER_BODY_HTML = "<style>body{font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;line-height:2.5rem;text-align:center;cursor:default;user-select:none;margin:0;overflow:hidden;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}a{width:3rem;height:2.5rem;position:absolute;text-decoration:none;color:inherit}b{display:block;cursor:pointer}table{width:100%;table-layout:fixed;border-spacing:0;border-collapse:collapse;text-align:center;line-height:2.5rem}td,th{padding:0}thead{background:#d3d3d3;font-size:smaller;font-weight:700}[aria-disabled=true],[aria-selected=false]{color:graytext}[aria-selected=true]{box-shadow:inset 0 0 0 1px graytext}[aria-disabled=true],[aria-selected=true],a:hover,td:hover{background-color:#f5f5f5}table+table{line-height:3.75rem;background:#fff;position:absolute;top:2.5em;left:0;opacity:1;transition:.1s ease-out}table+table[aria-hidden=true]{visibility:hidden;opacity:0}</style><a style=\"left:0\">&#x25C4;</a> <a style=\"right:0\">&#x25BA;</a> <b></b><table><thead>" + repeat(7, function (_, i) {
     return "<th>" + localeWeekday(i);
   }) + "</thead><tbody>" + repeat(7, "<tr>" + repeat(7, "<td>") + "</tr>") + "</tbody></table><table><tbody>" + repeat(3, function (_, i) {
@@ -117,8 +119,6 @@
       }
     },
     _initPicker: function _initPicker(svg, object, picker) {
-      var _this2 = this;
-
       var pickerRoot = DOM.constructor(object.contentDocument);
       var pickerBody = pickerRoot.find("body");
       pickerBody.set(PICKER_BODY_HTML);
@@ -130,7 +130,9 @@
 
       var resetValue = this._syncValue.bind(this, svg, picker, invalidatePicker, "defaultValue");
 
-      var updateValue = this._syncValue.bind(this, svg, picker, invalidatePicker, "value"); // patch value property on the original input
+      var updateValue = this._syncValue.bind(this, svg, picker, invalidatePicker, "value");
+
+      var toggleState = this._togglePicker.bind(this, picker, invalidatePicker); // patch value property for the input element
 
 
       var valueDescriptor = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, "value");
@@ -138,72 +140,49 @@
         configurable: false,
         enumerable: true,
         get: valueDescriptor.get,
-        set: function set(value) {
-          if (typeof value === "string") {
-            var dateValue = new Date(value);
-            var range = readDateRange(_this2);
-
-            if (dateValue < range[0]) {
-              value = formatISODate(range[0]);
-            } else if (dateValue > range[1]) {
-              value = formatISODate(range[1]);
-            } else if (isNaN(dateValue.getTime())) {
-              value = "";
-            }
-
-            valueDescriptor.set.call(_this2[0], value);
-            updateValue();
-          }
-        }
-      }); // define expanded property for the picker element
-
-      Object.defineProperty(picker[0], "expanded", {
-        configurable: false,
-        enumerable: true,
-        get: function get() {
-          return picker.get("aria-expanded") === "true";
-        },
-        set: function set(expanded) {
-          if (typeof expanded === "boolean") {
-            picker.set("aria-expanded", expanded);
-
-            if (expanded) {
-              calendarMonths.show();
-            } else {
-              calendarMonths.hide();
-            }
-
-            invalidatePicker(expanded);
-          }
-        }
+        set: this._setValue.bind(this, valueDescriptor.set, updateValue)
       }); // sync picker visibility on focus/blur
 
-      this.on("focus", this._focusPicker.bind(this, picker));
+      this.on("focus", this._focusPicker.bind(this, picker, toggleState));
       this.on("blur", this._blurPicker.bind(this, picker));
       this.on("change", updateValue);
-      this.on("keydown", ["which"], this._keydownPicker.bind(this, picker)); // form events do not trigger any state change
+      this.on("keydown", ["which"], this._keydownPicker.bind(this, picker, toggleState)); // form events do not trigger any state change
 
       this.closest("form").on("reset", resetValue); // picker invalidate handlers
 
       calenderDays.on("picker:invalidate", ["detail"], this._invalidateDays.bind(this, calenderDays));
       calendarMonths.on("picker:invalidate", ["detail"], this._invalidateMonths.bind(this, calendarMonths));
-      pickerBody.on("picker:invalidate", ["detail"], this._invalidateCaption.bind(this, calendarCaption, picker));
-      var clickEventName = "orientation" in window ? "touchend" : "mousedown"; // picker click handlers
+      pickerBody.on("picker:invalidate", ["detail"], this._invalidateCaption.bind(this, calendarCaption, picker)); // picker click handlers
 
-      pickerBody.on(clickEventName, "a", ["target"], this._clickPickerButton.bind(this, picker));
-      pickerBody.on(clickEventName, "td", ["target"], this._clickPickerDay.bind(this, picker));
-      calendarCaption.on(clickEventName, this._clickPickerCaption.bind(this, picker)); // prevent input from loosing the focus outline
+      pickerBody.on(CLICK_EVENT_TYPE, "a", ["target"], this._clickPickerButton.bind(this, picker));
+      pickerBody.on(CLICK_EVENT_TYPE, "td", ["target"], this._clickPickerDay.bind(this, picker, toggleState));
+      calendarCaption.on(CLICK_EVENT_TYPE, toggleState); // prevent input from loosing the focus outline
 
-      pickerBody.on(clickEventName, function () {
+      pickerBody.on(CLICK_EVENT_TYPE, function () {
         return false;
       });
-      this.on(clickEventName, this._focusPicker.bind(this, picker));
+      this.on(CLICK_EVENT_TYPE, this._focusPicker.bind(this, picker, toggleState));
       resetValue(); // present initial value
       // display calendar for autofocused elements
 
       if (DOM.get("activeElement") === this[0]) {
         picker.show();
       }
+    },
+    _setValue: function _setValue(setter, updateValue, value) {
+      var dateValue = new Date(String(value));
+      var range = readDateRange(this);
+
+      if (dateValue < range[0]) {
+        value = formatISODate(range[0]);
+      } else if (dateValue > range[1]) {
+        value = formatISODate(range[1]);
+      } else if (isNaN(dateValue.getTime())) {
+        value = "";
+      }
+
+      setter.call(this[0], value);
+      updateValue();
     },
     _invalidatePicker: function _invalidatePicker(calendarMonths, calenderDays, expanded, dateValue) {
       if (!dateValue) {
@@ -217,6 +196,12 @@
       var target = expanded ? calendarMonths : calenderDays; // refresh current picker
 
       target.fire("picker:invalidate", dateValue);
+
+      if (expanded) {
+        calendarMonths.show();
+      } else {
+        calendarMonths.hide();
+      }
     },
     _invalidateDays: function _invalidateDays(calenderDays, dateValue) {
       var month = dateValue.getUTCMonth();
@@ -271,7 +256,7 @@
     _invalidateCaption: function _invalidateCaption(calendarCaption, picker, dateValue) {
       var year = dateValue.getUTCFullYear(); // update calendar caption
 
-      if (picker.get("expanded")) {
+      if (picker.get("aria-expanded") === "true") {
         calendarCaption.value(year);
       } else {
         var month = dateValue.getUTCMonth();
@@ -295,14 +280,14 @@
 
       this.css("background-image", "url('data:image/svg+xml;utf8," + svg.get("outerHTML") + "')"); // update picker state
 
-      invalidatePicker(picker.get("expanded"), dateValue);
+      invalidatePicker(picker.get("aria-expanded") === "true", dateValue);
     },
     _clickPickerButton: function _clickPickerButton(picker, target) {
       var targetDate = new Date(this.value());
       if (isNaN(targetDate.getTime())) targetDate = new Date();
       var sign = target.next("a")[0] ? -1 : 1;
 
-      if (picker.get("expanded")) {
+      if (picker.get("aria-expanded") === "true") {
         targetDate.setUTCFullYear(targetDate.getUTCFullYear() + sign);
       } else {
         targetDate.setUTCMonth(targetDate.getUTCMonth() + sign);
@@ -310,10 +295,10 @@
 
       this.value(formatISODate(targetDate)).fire("change");
     },
-    _clickPickerDay: function _clickPickerDay(picker, target) {
+    _clickPickerDay: function _clickPickerDay(picker, toggleState, target) {
       var targetDate;
 
-      if (picker.get("expanded")) {
+      if (picker.get("aria-expanded") === "true") {
         if (isNaN(target._ts)) {
           targetDate = new Date();
         } else {
@@ -321,7 +306,7 @@
         } // switch to date calendar mode
 
 
-        picker.set("expanded", false);
+        toggleState(false);
       } else {
         if (!isNaN(target._ts)) {
           targetDate = new Date(target._ts);
@@ -333,10 +318,15 @@
         this.value(formatISODate(targetDate)).fire("change");
       }
     },
-    _clickPickerCaption: function _clickPickerCaption(picker) {
-      picker.set("expanded", !picker.get("expanded"));
+    _togglePicker: function _togglePicker(picker, invalidatePicker, force) {
+      if (typeof force !== "boolean") {
+        force = picker.get("aria-expanded") !== "true";
+      }
+
+      picker.set("aria-expanded", force);
+      invalidatePicker(force);
     },
-    _keydownPicker: function _keydownPicker(picker, which) {
+    _keydownPicker: function _keydownPicker(picker, toggleState, which) {
       if (which === VK_ENTER && picker.get("aria-hidden") === "true") {
         // ENTER key should submit form if calendar is hidden
         return true;
@@ -345,7 +335,7 @@
       if (which === VK_SPACE) {
         // SPACE key toggles calendar visibility
         if (!this.get("readonly")) {
-          picker.set("expanded", false);
+          toggleState(false);
 
           if (picker.get("aria-hidden") === "true") {
             picker.show();
@@ -359,7 +349,7 @@
         this.value("").fire("change"); // BACKSPACE, DELETE clear value
       } else if (which === VK_CONTROL) {
         // CONTROL toggles calendar mode
-        picker.set("expanded", !picker.get("expanded"));
+        toggleState();
       } else {
         var delta,
             currentDate = new Date(this.value());
@@ -376,7 +366,7 @@
         }
 
         if (delta) {
-          var expanded = picker.get("expanded");
+          var expanded = picker.get("aria-expanded") === "true";
 
           if (expanded && (which === 40 || which === 38)) {
             currentDate.setUTCMonth(currentDate.getUTCMonth() + (delta > 0 ? 4 : -4));
@@ -397,7 +387,7 @@
     _blurPicker: function _blurPicker(picker) {
       picker.hide();
     },
-    _focusPicker: function _focusPicker(picker) {
+    _focusPicker: function _focusPicker(picker, toggleState) {
       if (this.get("readonly")) return false;
       var offset = this.offset();
       var pickerOffset = picker.offset();
@@ -405,13 +395,13 @@
 
       if (HTML.clientHeight < offset.bottom + pickerOffset.height) {
         marginTop = -pickerOffset.height;
-      }
+      } // always reset picker mode to the default
 
-      picker // always reset picker mode to the default
-      .set("expanded", false) // always recalculate picker top position
-      .css("margin-top", marginTop) // show picker
-      .show();
+
+      toggleState(false); // always recalculate picker top position
+
+      picker.css("margin-top", marginTop).show();
     }
   });
 })(window.DOM, 32, 9, 13, 27, 8, 46, 17);
-DOM.importStyles("@media screen", ".btr-dateinput-picker{display:inline-block;vertical-align:bottom}.btr-dateinput-picker>object{width:21rem;max-height:calc(2.5rem*8);box-shadow:0 0 15px gray;background:white;position:absolute;opacity:1;-webkit-transform:translate3d(0,0,0);transform:translate3d(0,0,0);-webkit-transform-origin:0 0;transform-origin:0 0;transition:.1s ease-out}.btr-dateinput-picker[aria-hidden=true]>object{opacity:0;-webkit-transform:skew(-25deg) scaleX(.75);transform:skew(-25deg) scaleX(.75);visibility:hidden;height:0}.btr-dateinput-picker[aria-expanded=true]>object{max-height:calc(2.5rem + 2.5rem*1.5*3)}.btr-dateinput-picker+input{color:transparent!important}.btr-dateinput-picker+input::selection{background:transparent}.btr-dateinput-picker+input::-moz-selection{background:transparent}");
+DOM.importStyles("@media screen", "dateinput-picker{display:inline-block;vertical-align:bottom}dateinput-picker>object{width:21rem;max-height:calc(2.5rem*8);box-shadow:0 0 15px gray;background:white;position:absolute;opacity:1;-webkit-transform:translate3d(0,0,0);transform:translate3d(0,0,0);-webkit-transform-origin:0 0;transform-origin:0 0;transition:.1s ease-out}dateinput-picker[aria-hidden=true]>object{opacity:0;-webkit-transform:skew(-25deg) scaleX(.75);transform:skew(-25deg) scaleX(.75);visibility:hidden;height:0}dateinput-picker[aria-expanded=true]>object{max-height:calc(2.5rem + 2.5rem*1.5*3)}dateinput-picker+input{color:transparent!important;caret-color:transparent!important}dateinput-picker+input::selection{background:transparent}dateinput-picker+input::-moz-selection{background:transparent}");
