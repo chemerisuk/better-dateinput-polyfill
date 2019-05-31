0fba579e20b6c1728f26a5eff09fb2d8e9b180d6
Jane Weesner <jane.e.weesner@gmail.com>
2019-05-30 18:58:55 -0700

    lets allow early values too, of course

--- src/main.js
+++ src/main.js
@@ -110,8 +110,10 @@ DOM.extend("input[type=date]", {
         if (!dateValue) {
             value = "";
         } else {
-            const min = new Date(this.get("min") + "T00:00");
-            const max = new Date(this.get("max") ? this.get("max") + "T00:00" : '');
+            const minAttribute = this.get("min");
+            const maxAttribute = this.get("max")
+            const min = new Date(minAttribute ? minAttribute + "T00:00" : '');
+            const max = new Date(maxAttribute ? maxAttribute + "T00:00" : '');
 
             if (dateValue < min) {
                 value = formatLocalDate(min);
